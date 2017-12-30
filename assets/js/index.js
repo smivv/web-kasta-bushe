af3dca57f59ca3dc37960606232d0b8157923568
Vladimir Smirnov <vovkasmirnov@yahoo.com>
2017-12-30 11:08:44 +0300

    Background for mobile version added. Scripts refactored.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,45 @@
-window.addEventListener('load', function() {
-    var Preloader = function (element) {
-        var video =  document.getElementById(element),
-            preloader = document.getElementById('preloader'),
-            enter = document.getElementById('enter'),
-            trailer = document.getElementById('trailer');
+var video = $('#video'),
+    preloader = $('#preloader'),
+    enter = $('#enter'),
+    trailer = $('#trailer');
 
-        var api = {};
+var api = {
+    removePreloader: function() {
+        preloader.css('opacity', 0);
+        trailer.css('opacity', 1);
+        enter.css('opacity', 1);
+    },
 
-        api.removePreloader = function() {
-            preloader.style.opacity = 0;
-            trailer.style.opacity = 1;
-            enter.style.opacity = 1;
-        };
-
-        api.startCheckingLoading = function() {
-
-            video.addEventListener('click', function(){
-                video.play();
-            }, false);
-
-            video.addEventListener('play',
-                api.removePreloader()
-            );
-        };
-
-        var checker = function() {
-            var p = Math.round(video.buffered.end(0)) / Math.round(video.seekable.end(0));
+    startCheckingLoading: function() {
+        video.on('click', function () {
+            video.play();
+        }).on('play', function () {
+            api.removePreloader();
+        }).on('progress', function() {
+            var p = Math.round(video[0].buffered.end(0)) /
+                Math.round(video[0].seekable.end(0));
             console.log(p);
             if (p > 0.3) {
                 api.removePreloader();
-                video.removeEventListener("progress", checker, false);
+                video.off('progress');
             }
-        };
+        });
+    }
+};
 
-        video.addEventListener("progress", checker, false);
+if(bowser.mobile){
+    video.remove();
+    setTimeout(api.removePreloader(), 3000);
+    console.log('mobile');
+}else{
+    $('body').css('background-image', 'url()');
+    $(document).ready(function () {
+        api.startCheckingLoading();
+    });
+}
 
-        if(bowser.android || bowser.ios){
-            video.setAttribute('controls', 'controls');
-        }
 
-        return api;
-    };
-
-    Preloader('video').startCheckingLoading();
+$(document).ready(function () {
 
     $(".watch_trailer").modalVideo();
 });
-
